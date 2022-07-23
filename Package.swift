47b560f7115bc66e2dbecd457aef4a90c2416ea1
Max Desiatov <max@desiatov.com>
2022-07-23 20:34:38 +0100

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -10,12 +10,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TokamakVapor",
-            targets: ["TokamakVapor"]),
+            targets: ["TokamakVapor"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/TokamakUI/Tokamak.git", from: "0.6.1"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.57.1"),
+        .package(url: "https://github.com/TokamakUI/Tokamak.git", from: "0.10.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,15 +25,15 @@ let package = Package(
             name: "TokamakVapor",
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
-                .product(name: "TokamakStaticHTML", package: "Tokamak")
+                .product(name: "TokamakStaticHTML", package: "Tokamak"),
             ]
         ),
         .testTarget(
             name: "TokamakVaporTests",
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
-                "TokamakVapor"
+                "TokamakVapor",
             ]
-        )
+        ),
     ]
 )
